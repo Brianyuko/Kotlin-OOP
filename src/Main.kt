07c0c2a89b5c0018e544417c3cd9ba8530b7be22
Brianyuko <brianyuko45@gmail.com>
2020-07-27 05:50:28 +0700

    Object Person2

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    // NameClass(Set Value)
+    val person1 = User("Robi", 50)
+    // Setter Name
+    person1.name = "Ahmad dimas"
+    val person2 = User("Makmur", 20, " Jaya")
+    println("Nama saya: ${person1.name}\nUmur: ${person1.age} ")
+    println("Nama saya: ${person2.name}")
+}

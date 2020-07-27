fe65d1659abafbb64d405c09abae7eac00d0e75f
Brianyuko <brianyuko45@gmail.com>
2020-07-28 00:40:27 +0700

    Setter getter

--- src/User.kt
+++ src/User.kt
@@ -1,12 +1,19 @@
 // Object Oriented Programming
 
 // class NameClass (Constructor)
-class User(var name: String, var age: Int){
+class User(var firstName: String, var lastName: String){
     //var name: String = name
     //var age: Int = age
 
     //Secondary Constructor
-    constructor(name: String, age: Int, lastName: String): this(name, age){
-        this.name = name + lastName
-    }
+//    constructor(name: String, age: Int, lastName: String): this(name, age){
+//        this.name = name + lastName
+//    }
+    var name: String
+        get() = firstName +" "+ lastName
+        set(value) {
+            val fullName = value.split(" ")
+            firstName = fullName[0]
+            lastName = fullName[1]
+        }
 }

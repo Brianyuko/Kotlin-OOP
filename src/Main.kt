9326ff3d2df8388a1b775250200d5da685193dda
Brianyuko <brianyuko45@gmail.com>
2020-07-28 00:49:41 +0700

    Init

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
 fun main(){
     // NameClass(Set Value)
-    val person1 = User("Robi", "50")
+    val person1 = User("Robi", "50", 0)
     // Setter Name
     person1.name = "Ahmad dimas"
     //val person2 = User("Makmur", 20, " Jaya")
     //println("Nama saya: ${person1.name}\nUmur: ${person1.age} ")
     //println("Nama saya: ${person2.name}")
-    val person3 = User("Robi", "Purba")
+    val person3 = User("Robi", "Purba", 20)
     person3.name = "Ahmad Santoso"
-    println("Nama saya ${person3.name}")
+    println("Nama saya ${person3.name} umur ${person3.age}")
 }

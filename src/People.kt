05c03f48f43dfb206c57d7f49deb8f6404ffb451
Brianyuko <brianyuko45@gmail.com>
2020-08-07 09:47:06 +0700

    Extension Function

--- src/People.kt
+++ src/People.kt
@@ -8,6 +8,9 @@ class People {
 //            println(nama) -> unresolved, bcs nama private val
 //        }
 //    }
+    fun cekAngka(x:Int) : Boolean{
+        return x>10
+    }
     // Inner class can access private scope
     inner class UserName {
         fun getName(){

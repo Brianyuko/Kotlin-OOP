4f37b7adb2e8982f7e878bf38dd762df2ddbe5c7
Brianyuko <brianyuko45@gmail.com>
2020-08-07 10:17:26 +0700

    Object Declarations

---
+++ src/Car.kt
@@ -0,0 +1,11 @@
+object Car {
+    private var heightCar : Int = 21
+    fun printHeight(){
+        println("Height car is $heightCar")
+    }
+}
+
+fun main(){
+    // Object Declarations
+    Car.printHeight()
+}
